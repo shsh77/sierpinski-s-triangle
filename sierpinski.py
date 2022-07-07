2d28d55ab80427b8c76e311a5d7b5557cf2fac69
shsh77 <104766299+shsh77@users.noreply.github.com>
2022-07-07 21:08:48 +0100

    Update sierpinski.py

--- sierpinski.py
+++ sierpinski.py
@@ -1,5 +1,5 @@
 import turtle as t
-t.speed(5)
+t.speed(100)
 t.pu()
 t.rt(150)
 t.fd(200)
@@ -10,23 +10,22 @@ n=0
 global lay
 lay=[]
 def findmove(s):
-  print(f"findmove s={s}")
   if s==0:
     lay.clear()
   if s < i:
     for q in range(3):
       findmove(s+1)
   lay.append(800/2**s)
-def centretri():
-  t.fd(200/2**i)
+def centretri(size):
+  t.fd(200/2**size)
   t.lt(60)
   t.begin_fill()
   for p in range(3):
-    t.fd(200/2**i)
+    t.fd(200/2**size)
     t.lt(120)
   t.end_fill()
   t.lt(120)
-  t.fd(200/2**i)
+  t.fd(200/2**size)
   t.lt(180)
 for k in range(3):
   t.fd(400)
@@ -34,12 +33,12 @@ for k in range(3):
 while 1: 
   findmove(0)
   lay.remove(800)
-  centretri()
+  centretri(i)
   if not len(lay) == 0:
     t.fd(lay.pop(0))
     t.lt(120)
   while not len(lay) == 0:
-    centretri()
+    centretri(i)
     t.fd(lay.pop(0))
     t.lt(120)
   i+=1
